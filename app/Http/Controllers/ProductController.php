b002844e3a9b4f2c86111c2b9eaacbf580eaa78a
rikky_r007 <rikkyr0071@gmail.com>
2019-12-18 11:34:22 +0700

    checkpoint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
         $productlist = ProductModel::all();
         return Datatables::of($productlist)
          ->addColumn('action', function ($productlist) {
-                $btn = '<center><a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$productlist->products_id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct"><i class="fa fa-edit"></i>&nbspEdit</a>';
+                $btn = '<center><a href="/products/edit/{{ $productlist->products_id }}" data-toggle="tooltip"  data-id="'.$productlist->products_id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct"><i class="fa fa-edit"></i>&nbspEdit</a>';
                            $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$productlist->products_id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct"><i class="fa fa-trash"></i>&nbspDelete</a></center>';
                       return $btn;
             }
