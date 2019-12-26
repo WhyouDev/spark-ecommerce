85ec66c4412eff642efe1fec052aff730a5695f0
sarongTech <ikhawahyuramadhan@gmail.com>
2019-12-26 16:27:22 +0700

    up

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,9 @@
 <div class="hero container">
     <div class="hero-copy">
         <h1>Ecommerce</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, ullam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nihil commodi incidunt eum vel, 
+            libero dolores facere, aliquam, ratione esse saepe. 
+            Placeat illum temporibus magni corporis quis quas facere? Adipisci.</p>
         <div class="hero-buttons">
             <a href="#" class="button button-black">GitHub</a>
             {{-- <a href="#" class="button button-black"></a> --}}
